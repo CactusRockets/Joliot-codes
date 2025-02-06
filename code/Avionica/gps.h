0c7d69d23ec629177cacc834309ad87e0b844e1b
Daniel-Alencar <danielalencar746@gmail.com>
2025-02-05 22:26:02 -0300

    GPS module communication

--- code/Avionica/gps.h
+++ code/Avionica/gps.h
@@ -1,86 +1,79 @@
-#include <TinyGPSPlus.h>
+#include <TinyGPS++.h>
 #include <HardwareSerial.h>
-
-/* CONFIGURAÇÕES GPS */
-#define GPS_WAY 1
 
 // Pinos da Serial 1 do ESP32
 #define RX1_PIN 15
 #define TX1_PIN 4
 
-// Usando a Serial 1 do ESP32
-HardwareSerial GPSSerial(1);
 TinyGPSPlus gps;
+// Usando a Serial 1 do ESP32
+HardwareSerial gpsSerial(1);
 
 String gpsData;
+int gps_available;
 
 double latitude = 0, longitude = 0;
-int ano, mes, dia, hora, minuto, segundo, centesimo;
+int ano, mes, dia, hora, minuto, segundo;
 
 void setupGPS() {
-  GPSSerial.begin(9600, SERIAL_8N1, RX1_PIN, TX1_PIN);
-  while(!GPSSerial);
+  gpsSerial.begin(9600, SERIAL_8N1, RX1_PIN, TX1_PIN);
   println("GPS conectado!");
 }
 
-void getLatitudeAndLongitude() {
-  if(GPSSerial.available()) {
-    // Lê os dados recebidos do módulo GPS
-    gpsData = GPSSerial.readStringUntil('\n');
-    
-    // Verifica se os dados começam com "$GPGGA", que é 
-    // uma sentença NMEA que contém as informações 
-    // de latitude e longitude
-    if(gpsData.startsWith("$GPGGA")) {
-
-      // Divide os dados em campos usando a vírgula como separador
-      // Os campos estão na seguinte ordem: 
-      // $GPGGA, horário, latitude, N/S, longitude, E/W, qualidade do sinal GPS, número de satélites
-      // No nosso caso, queremos o campo de latitude (índice 2) e o campo de longitude (índice 4)
-      String fields[15];
-      int fieldIndex = 0;
-      int startPos = 0;
-      int commaPos;
-      
-      // Loop para dividir os campos separados por vírgula
-      while((commaPos = gpsData.indexOf(',', startPos)) != -1) {
-        fields[fieldIndex] = gpsData.substring(startPos, commaPos);
-        fieldIndex++;
-        startPos = commaPos + 1;
-      }
-      
-      // Extrai os valores de latitude e longitude e converte-os em valores numéricos
-      latitude = fields[2].toFloat();
-      longitude = fields[4].toFloat();
-    }
-  }
-}
-
 void getGPSData() {
-  bool isNewGPSData = false;
+  while(true) {
+    gps_available = gpsSerial.available();
+    // Serial.println(gps_available);
 
-  while(GPSSerial.available() > 0) {
-    char GPSData = GPSSerial.read();
-    isNewGPSData = gps.encode(GPSData);
-  }
+    if(gps_available <= 0) {
+      break;
+    }
+    // Processa os dados recebidos do GPS
+    gps.encode(gpsSerial.read());
 
-  if(isNewGPSData) {
-    if(gps.location.isValid()) {
-      latitude = gps.location.lat();
-      longitude = gps.location.lng();
+    // Se houver uma nova atualização de posição
+    if (gps.location.isUpdated()) {
+        latitude = gps.location.lat();
+        longitude = gps.location.lng();
+
+        // Serial.print("Latitude: ");
+        // Serial.print(gps.location.lat(), 6);
+        // Serial.print(" | Longitude: ");
+        // Serial.println(gps.location.lng(), 6);
     }
 
-    if(gps.date.isValid()) {
-      mes = gps.date.month();
-      dia = gps.date.day();
-      ano = gps.date.year();
+    // Se houver atualização da data
+    if (gps.date.isUpdated()) {
+        dia = gps.date.day();
+        mes = gps.date.month();
+        ano = gps.date.year();
+
+        // Serial.print("Data: ");
+        // Serial.print(gps.date.day());
+        // Serial.print("/");
+        // Serial.print(gps.date.month());
+        // Serial.print("/");
+        // Serial.println(gps.date.year());
     }
-    
-    if(gps.time.isValid()) {
-      hora = gps.time.hour();
-      minuto = gps.time.minute();
-      segundo = gps.time.second();
-      centesimo = gps.time.centisecond();
+
+    // Se houver atualização do horário
+    if (gps.time.isUpdated()) {
+        hora = gps.time.hour();
+        minuto = gps.time.minute();
+        segundo = gps.time.second();
+
+        // Serial.print("Hora: ");
+        // Serial.print(gps.time.hour());
+        // Serial.print(":");
+        // Serial.print(gps.time.minute());
+        // Serial.print(":");
+        // Serial.println(gps.time.second());
+    }
+
+    // Se houver atualização da velocidade
+    if (gps.speed.isUpdated()) {
+        // Serial.print("Velocidade (km/h): ");
+        // Serial.println(gps.speed.kmph());
     }
   }
 }
@@ -112,10 +105,7 @@ void printData() {
 }
 
 void updateGPSData() {
-  if(GPS_WAY == 1) {
-    getGPSData();
-  } else if(GPS_WAY == 2) {
-    getLatitudeAndLongitude();
-  }
+  getGPSData();
   saveGPSData();
+  printData();
 }
